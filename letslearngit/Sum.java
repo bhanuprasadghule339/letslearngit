fae30d297e72e3531b281447380126545046b18c
Bhanuprasad Ghule <bhanuprasadghule333@gmail.com>
2021-11-29 09:36:00 +0530

    adding massege \n sum.java

--- letslearngit/Sum.java
+++ letslearngit/Sum.java
@@ -3,6 +3,6 @@ class Sum{
     public static void main(String args[])
     {
         int a=1,b=3;
-        System.out.println("The sum of the numbers is:"+a+b);
+        System.out.println(a+b);
     }
 }
